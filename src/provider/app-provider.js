383f4238477337e52db599b21f08e4ea772504b0
Nha NHV <nguyenhoangvannha@gmail.com>
2020-06-13 18:44:17 +0700

    Add SkillsProvider

--- src/provider/app-provider.js
+++ src/provider/app-provider.js
@@ -3,16 +3,20 @@ import { AuthenticationProvider } from './authentication-provider';
 import { ThemeProvider } from './theme-provider';
 import { CoursesProvider } from './courses-provider'
 import { FavouritesProvider } from './favourites-provider';
+import { SkillsProvider } from './skills-provider';
 const AppContext = React.createContext();
 
 const AppProvider = (props) => {
+
     return (
         <AppContext.Provider>
             <AuthenticationProvider>
                 <ThemeProvider>
                     <CoursesProvider>
                         <FavouritesProvider>
-                            {props.children}
+                            <SkillsProvider>
+                                {props.children}
+                            </SkillsProvider>
                         </FavouritesProvider>
                     </CoursesProvider>
                 </ThemeProvider>
--- src/provider/courses-provider.js
+++ src/provider/courses-provider.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {coursesData} from '../data/mock/courses-mock-data'
+import { coursesData } from '../data/mock/courses-mock-data'
 
 const CoursesContext = React.createContext()
 
@@ -7,8 +7,22 @@ const CoursesProvider = (props) => {
 
     const [courses, setCourses] = useState(coursesData)
 
+    const [learningCourseIds, setLearningCourseIds] = useState([])
+
+    const [newCourseIds, setNewCourseIds] = useState([])
+
+    const [recommendedCourseIds, setRecommendedCourseIds] = useState([])
+
     return (
-        <CoursesContext.Provider value={{courses, setCourses}}>
+        <CoursesContext.Provider
+            value={{
+                courses,
+                setCourses,
+                learningCourseIds,
+                setLearningCourseIds,
+                recommendedCourseIds,
+                setRecommendedCourseIds,
+            }}>
             {props.children}
         </CoursesContext.Provider>
     )
