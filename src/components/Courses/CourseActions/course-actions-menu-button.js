85659194c9d6e279ad319bb333a7bdb3343fbb03
Nha NHV <nguyenhoangvannha@gmail.com>
2020-08-23 10:32:32 +0700

    Remove Favourite

--- src/components/Courses/CourseActions/course-actions-menu-button.js
+++ src/components/Courses/CourseActions/course-actions-menu-button.js
@@ -1,29 +1,52 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { StyleSheet } from 'react-native'
 import MenuButton from '../../Common/Menu/menu-button'
 import CMenuItem from '../../Common/Menu/c-menu-item'
 import IconName from '../../../res/icon-name'
 import i18n from '../../../res/i18n'
-import { CoursesContext } from '../../../provider/courses-provider'
-import { FavouritesContext } from '../../../provider/favourites-provider'
+import { useSelector, useDispatch } from 'react-redux'
+import { Status } from '../../../core/status'
+import { DO_ADD_FAVOURITE_COURSE_USER_ACTION, DoAddFavouriteCourseUserAction } from '../../../feature/user/actions'
 
 const CourseActionsMenuButton = ({ courseId }) => {
-    const courseState = useContext(CoursesContext)
-    const favouritesContext = useContext(FavouritesContext)
-    var isFavourite = favouritesContext.favouriteCourses.has(courseId ?? '') ?? false;
+
+    const [addFavouriteStatus, setAddFavouriteStatus] = useState(Status.idle())
+
+    const courseState = useSelector(state => state.courseState)
+
+    const userState = useSelector(state => state.userState)
+
+    const dispatch = useDispatch();
+
+    const [favourite, setFavourite] = useState(false)
+
+
+    useEffect(() => {
+
+        setAddFavouriteStatus(userState.status[`${DO_ADD_FAVOURITE_COURSE_USER_ACTION}${courseId}`])
+
+        courseState.favouriteCourses.forEach((value) => {
+            if (value.id === courseId) setFavourite(true)
+        })
+
+        return () => {
+            //cleanup
+        }
+    }, [userState, courseState])
+
+    const onPressFavourite = () => {
+        dispatch(DoAddFavouriteCourseUserAction(courseId))
+    }
+
 
     return (
         <MenuButton
             menuOptions={[
                 <CMenuItem
-                    title={isFavourite ? i18n.t('remove_favourite') : i18n.t('favourite')}
+                    title={favourite ? i18n.t('remove_favourite') : i18n.t('favourite')}
                     iconName={IconName.mdHeart}
                     onPress={
-                        () => {
-                            isFavourite ?
-                                favouritesContext.removeFavouriteCourse(courseId)
-                                : favouritesContext.addFavouriteCourse(courseId)
-                        }
+                        () => onPressFavourite()
                     }
                 />,
                 <CMenuItem title={i18n.t('add_to_channel')} iconName={IconName.iosRadio} />,
